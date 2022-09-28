66cb38e7a483787d7cf2fa357e112475067ddca4
Alexander Angelov <alexander.angelov@mentormate.com>
2022-09-28 22:21:05 +0300

    Concentration game test finished

---
+++ ConcentrationGame/Card.swift
@@ -0,0 +1,37 @@
+//
+//  Card.swift
+//  ConcentrationGame
+//
+//  Created by Alexander Angelov on 20.09.22.
+//
+
+import Foundation
+
+
+struct Card: Hashable
+{
+    //var hashValue: Int { return identifier}
+    func hash(into hasher: inout Hasher) {
+            hasher.combine(identifier)
+        }
+   
+    static func == (lhs: Card, rhs: Card) -> Bool {
+      return lhs.identifier == rhs.identifier
+    }
+    
+    var wasFlipped = false
+    var isFaceUp = false
+    var isMatched = false
+    private var identifier: Int
+    
+    private static var identifierFactory = 0
+    private static func getIdentifier() -> Int{
+        identifierFactory += 1
+        return identifierFactory
+    }
+    
+    init(){
+        self.identifier = Card.getIdentifier()
+    }
+    
+}
